62d7de550caa13b8a57e80b0ed96209ae901c252
Pridesd <whwotjr98@naver.com>
2025-05-21 21:51:27 +0900

    feat: enhance Text component with variant, size, and font props for better customization

--- src/components/text.tsx
+++ src/components/text.tsx
@@ -1,11 +1,60 @@
+import clsx from "clsx"
 import { ElementType, HTMLAttributes, PropsWithChildren } from "react"
 
+type TextVariant = "heading" | "body" | "description"
+type TextSize = "small" | "medium" | "large"
+type TextFont = "pretendard" | "Ownglyph ryurue"
+
 interface TextProps extends PropsWithChildren, HTMLAttributes<HTMLElement> {
-  as: ElementType
+  variant: TextVariant
+  size?: TextSize
+  font?: TextFont
+  as?: ElementType
 }
 
-export const Text = ({ children, as = "span", ...props }: TextProps) => {
-  const Component = as
+const TEXT_VARIANTS: Record<
+  TextVariant,
+  Record<TextSize, HTMLAttributes<HTMLElement>["className"]>
+> = {
+  heading: {
+    small: "text-xl font-bold",
+    medium: "text-2xl font-bold",
+    large: "text-[1.75rem] font-bold leading-9",
+  },
+  body: {
+    small: "text-sm font-semibold",
+    medium: "text-base font-semibold",
+    large: "text-lg font-semibold",
+  },
+  description: {
+    small: "text-[0.625rem] font-semibold",
+    medium: "text-[0.6875rem] font-semibold",
+    large: "text-xs font-semibold",
+  },
+}
 
-  return <Component {...props}>{children}</Component>
+const TEXT_FONT: Record<TextFont, HTMLAttributes<HTMLElement>["className"]> = {
+  pretendard: "font-pretendard",
+  "Ownglyph ryurue": "font-ryurue",
+}
+
+export const Text = ({
+  as,
+  children,
+  variant,
+  font = "pretendard",
+  size = "medium",
+  className,
+  ...props
+}: TextProps) => {
+  const Component = as || "span"
+
+  return (
+    <Component
+      className={clsx(className, TEXT_VARIANTS[variant][size], TEXT_FONT[font])}
+      {...props}
+    >
+      {children}
+    </Component>
+  )
 }
