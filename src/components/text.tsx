19fe0daff28566bafe953768f8152bb2c0eef0a9
Pridesd <whwotjr98@naver.com>
2025-05-21 21:06:56 +0900

    feat: refactor Text component to accept dynamic element types and children

--- src/components/text.tsx
+++ src/components/text.tsx
@@ -1,3 +1,11 @@
-export const Text = () => {
-  return <>Text</>
+import { ElementType, HTMLAttributes, PropsWithChildren } from "react"
+
+interface TextProps extends PropsWithChildren, HTMLAttributes<HTMLElement> {
+  as: ElementType
+}
+
+export const Text = ({ children, as = "span", ...props }: TextProps) => {
+  const Component = as
+
+  return <Component {...props}>{children}</Component>
 }
