8b9588913248e54cf93ea16eb30f8430c17ae648
Pridesd <whwotjr98@naver.com>
2025-05-23 01:08:38 +0900

    feat: enhance Text component with additional color options and improved structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,31 +33,195 @@ export default function Home() {
         </Text>
       </div>
       <div className="flex flex-col gap-1">
-        <Text variant="heading" size="large" font="Ownglyph ryurue">
+        <Text
+          variant="heading"
+          size="large"
+          font="Ownglyph ryurue"
+          color="primary"
+        >
           안녕하세요
         </Text>
-        <Text variant="heading" size="medium" font="Ownglyph ryurue">
+        <Text
+          variant="heading"
+          size="medium"
+          font="Ownglyph ryurue"
+          color="secondary"
+        >
           안녕하세요
         </Text>
-        <Text variant="heading" size="small" font="Ownglyph ryurue">
+        <Text
+          variant="heading"
+          size="small"
+          font="Ownglyph ryurue"
+          color="tertiary"
+        >
           안녕하세요
         </Text>
-        <Text variant="body" size="large" font="Ownglyph ryurue">
+        <Text
+          variant="body"
+          size="large"
+          font="Ownglyph ryurue"
+          color="assistive"
+        >
           안녕하세요
         </Text>
-        <Text variant="body" size="medium" font="Ownglyph ryurue">
+        <Text
+          variant="body"
+          size="medium"
+          font="Ownglyph ryurue"
+          color="disabled"
+        >
           안녕하세요
         </Text>
-        <Text variant="body" size="small" font="Ownglyph ryurue">
+        <Text
+          variant="body"
+          size="small"
+          font="Ownglyph ryurue"
+          color="inverse"
+        >
           안녕하세요
         </Text>
-        <Text variant="description" size="large" font="Ownglyph ryurue">
+        <Text
+          variant="description"
+          size="large"
+          font="Ownglyph ryurue"
+          color="brand"
+        >
           안녕하세요
         </Text>
-        <Text variant="description" size="medium" font="Ownglyph ryurue">
+        <Text
+          variant="description"
+          size="medium"
+          font="Ownglyph ryurue"
+          color="neutral-10"
+        >
           안녕하세요
         </Text>
-        <Text variant="description" size="small" font="Ownglyph ryurue">
+        <Text
+          variant="description"
+          size="small"
+          font="Ownglyph ryurue"
+          color="neutral-20"
+        >
+          안녕하세요
+        </Text>
+      </div>
+      <div className="flex flex-col gap-1">
+        <Text variant="heading" size="large" color="neutral-30">
+          안녕하세요
+        </Text>
+        <Text variant="heading" size="medium" color="neutral-40">
+          안녕하세요
+        </Text>
+        <Text variant="heading" size="small" color="neutral-50">
+          안녕하세요
+        </Text>
+        <Text variant="body" size="large" color="neutral-60">
+          안녕하세요
+        </Text>
+        <Text variant="body" size="medium" color="neutral-70">
+          안녕하세요
+        </Text>
+        <Text variant="body" size="small" color="neutral-80">
+          안녕하세요
+        </Text>
+        <Text variant="description" size="large" color="neutral-90">
+          안녕하세요
+        </Text>
+        <Text variant="description" size="medium" color="orange-10">
+          안녕하세요
+        </Text>
+        <Text variant="description" size="small" color="orange-50">
+          안녕하세요
+        </Text>
+      </div>
+      <div className="flex flex-col gap-1">
+        <Text
+          variant="heading"
+          size="large"
+          font="Ownglyph ryurue"
+          color="orange-100"
+        >
+          안녕하세요
+        </Text>
+        <Text
+          variant="heading"
+          size="medium"
+          font="Ownglyph ryurue"
+          color="green-10"
+        >
+          안녕하세요
+        </Text>
+        <Text
+          variant="heading"
+          size="small"
+          font="Ownglyph ryurue"
+          color="green-50"
+        >
+          안녕하세요
+        </Text>
+        <Text
+          variant="body"
+          size="large"
+          font="Ownglyph ryurue"
+          color="green-100"
+        >
+          안녕하세요
+        </Text>
+        <Text
+          variant="body"
+          size="medium"
+          font="Ownglyph ryurue"
+          color="blue-10"
+        >
+          안녕하세요
+        </Text>
+        <Text
+          variant="body"
+          size="small"
+          font="Ownglyph ryurue"
+          color="blue-50"
+        >
+          안녕하세요
+        </Text>
+        <Text
+          variant="description"
+          size="large"
+          font="Ownglyph ryurue"
+          color="blue-100"
+        >
+          안녕하세요
+        </Text>
+        <Text
+          variant="description"
+          size="medium"
+          font="Ownglyph ryurue"
+          color="red-100"
+        >
+          안녕하세요
+        </Text>
+        <Text
+          variant="description"
+          size="small"
+          font="Ownglyph ryurue"
+          color="alpha-20"
+        >
+          안녕하세요
+        </Text>
+        <Text
+          variant="description"
+          size="small"
+          font="Ownglyph ryurue"
+          color="alpha-40"
+        >
+          안녕하세요
+        </Text>
+        <Text
+          variant="description"
+          size="small"
+          font="Ownglyph ryurue"
+          color="alpha-60"
+        >
           안녕하세요
         </Text>
       </div>
