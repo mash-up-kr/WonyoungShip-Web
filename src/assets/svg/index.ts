92759ebd9d58983b1dc0b77357d8282a354df9c8
Pridesd <whwotjr98@naver.com>
2025-05-29 17:23:32 +0900

    feat: add Clear SVG icon to ICONS export

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,8 +1,10 @@
 import ChevronDown from "./chevron-down.svg"
+import Clear from "./clear.svg"
 import TooltipArrow from "./tooltip-arrow.svg"
 
 export const ICONS = {
   chevronDown: ChevronDown,
+  clear: Clear,
   tooltipArrow: TooltipArrow,
 }
 
