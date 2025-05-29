e6422900a506af828e90d60f37a6e669f667460e
Pridesd <whwotjr98@naver.com>
2025-05-29 17:11:15 +0900

    feat: add TooltipArrow SVG icon to ICONS export

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,7 +1,9 @@
 import ChevronDown from "./chevron-down.svg"
+import TooltipArrow from "./tooltip-arrow.svg"
 
 export const ICONS = {
   chevronDown: ChevronDown,
+  tooltipArrow: TooltipArrow,
 }
 
 export type IconName = keyof typeof ICONS
