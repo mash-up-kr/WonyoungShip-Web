7f0c262ba51cc839192c85a202689d113bd7d049
Sinji <sinzi031012@naver.com>
2025-06-02 02:05:36 +0900

    feat: create WriteLetterButton

---
+++ src/app/home/_components/WriteLetterButton.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link"
+
+import { Text, Icon } from "@/components/common"
+
+export const WriteLetterButton = () => {
+  return (
+    <Link
+      href="/"
+      className="flex bg-background-white mx-2 w-full justify-between items-center rounded-2xl p-4"
+    >
+      <div className="flex flex-col justify-between">
+        <Text
+          variant="description"
+          size="large"
+          color="tertiary"
+          className="font-medium"
+        >
+          미래의 나에게 하고 싶은 말을 전해보세요
+        </Text>
+        <span className="from-neutral-70 bg-gradient-to-r to-blue-100 bg-clip-text text-lg font-semibold text-transparent">
+          미래의 나에게 편지 전하기
+        </span>
+      </div>
+      {/* TODO : icon size 회의 후 18px로 맞추기 */}
+      <Icon icon="chevronDown" fill="secondary" />
+    </Link>
+  )
+}
---
+++ src/app/home/page.tsx
@@ -0,0 +1,11 @@
+// TODO : 현봉 오빠 layout merge 하면 파일 레이아웃에 맞춰 옮기기
+
+import { WriteLetterButton } from "./_components/WriteLetterButton"
+
+export default function Home() {
+  return (
+    <div className="flex px-2 pt-2 pb-6">
+      <WriteLetterButton />
+    </div>
+  )
+}
