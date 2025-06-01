e176b0903dff4fe6e97fdfeb17725331477915ab
장현봉 <wkdgusqhd080@naver.com>
2025-06-01 20:58:17 +0900

    chore: remove button className template literal

--- src/components/common/button.tsx
+++ src/components/common/button.tsx
@@ -15,10 +15,12 @@ interface ButtonProps
 }
 
 const BUTTON_VARIANTS: Record<ButtonProps["variant"], string> = {
-  primary: `bg-blue-100 w-full max-w-[343px]`,
+  primary: "bg-blue-100 w-full max-w-[343px]",
 }
-const BUTTON_BASE_STYLES: HTMLAttributes<HTMLButtonElement>["className"] = `cursor-pointer rounded-[12px] py-[16px] text-white disabled:cursor-auto`
-const BUTTON_DISABLED_STYLES: HTMLAttributes<HTMLButtonElement>["className"] = `bg-neutral-20 text-text-tertiary`
+const BUTTON_BASE_STYLES: HTMLAttributes<HTMLButtonElement>["className"] =
+  "cursor-pointer rounded-[12px] py-[16px] text-white disabled:cursor-auto"
+const BUTTON_DISABLED_STYLES: HTMLAttributes<HTMLButtonElement>["className"] =
+  "bg-neutral-20 text-text-tertiary"
 
 export const Button = ({
   children,
