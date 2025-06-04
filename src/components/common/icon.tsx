2c64d5c9d8f91e47a9a41bea45f9b3667daca086
Pridesd <whwotjr98@naver.com>
2025-06-04 19:42:19 +0900

    feat: add 'xs' size option for icons

--- src/components/common/icon.tsx
+++ src/components/common/icon.tsx
@@ -10,7 +10,7 @@ type IconColor =
   | "inverse"
   | "brand"
 
-type IconSize = "sm" | "md" | "lg" | "xl"
+type IconSize = "xs" | "sm" | "md" | "lg" | "xl"
 
 interface IconProps {
   icon: IconName
@@ -43,6 +43,7 @@ const STROKE_COLORS: Record<IconColor, string> = {
 
 //TODO: 임의로 지정한 사이즈임. 디자인 시스템에 따라 조정 필요
 const ICON_SIZES: Record<IconSize, string> = {
+  xs: "w-3 h-3", // 12px
   sm: "w-4 h-4", // 16px
   md: "w-5 h-5", // 20px
   lg: "w-6 h-6", // 24px
