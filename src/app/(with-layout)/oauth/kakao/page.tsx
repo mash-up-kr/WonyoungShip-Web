6c4d47df17102b7ae538ac0428afa2b46a517f1e
Pridesd <whwotjr98@naver.com>
2025-06-10 21:35:02 +0900

    feat: implement Kakao OAuth code retrieval and update rendering

--- src/app/(with-layout)/oauth/kakao/page.tsx
+++ src/app/(with-layout)/oauth/kakao/page.tsx
@@ -1,9 +1,16 @@
 "use client"
 
-const KakaoOauthPage = () => {
-  console.log("KakaoOauthPage")
+import { useSearchParams } from "next/navigation"
 
-  return <>KakaoOauthPage</>
+const KakaoOauthPage = () => {
+  // TODO: 카카오 로그인 API 연동 및 로딩 화면 처리
+  const searchParams = useSearchParams()
+
+  const code = searchParams.get("code")
+
+  console.log("Kakao OAuth code:", code)
+
+  return <>{code}</>
 }
 
 export default KakaoOauthPage
