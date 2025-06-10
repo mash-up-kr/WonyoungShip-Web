a60a449c308d04262d6c675e14285c1f76f7aa96
Pridesd <whwotjr98@naver.com>
2025-06-10 21:22:56 +0900

    feat: add Kakao login button component with logo

--- src/components/landing/kakao-login-button.tsx
+++ src/components/landing/kakao-login-button.tsx
@@ -1,3 +1,16 @@
+import KakaoLogo from "@/assets/svg/kakao-logo.svg"
+
+import { Text } from "../common"
+
 export const KakaoLoginButton = () => {
-  return <>KakaoLoginButton</>
+  return (
+    <div className="flex justify-center">
+      <button className="flex cursor-pointer items-center justify-center gap-2">
+        <KakaoLogo />
+        <Text variant="body" size="small">
+          간편 로그인하기
+        </Text>
+      </button>
+    </div>
+  )
 }
