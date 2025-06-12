88472d6a07888e4447e249062564ba997317aa82
Pridesd <whwotjr98@naver.com>
2025-06-13 08:43:46 +0900

    refactor: remove unused Kakao OAuth code handling

--- src/app/(with-layout)/oauth/kakao/page.tsx
+++ src/app/(with-layout)/oauth/kakao/page.tsx
@@ -1,16 +1,7 @@
-"use client"
-
-import { useSearchParams } from "next/navigation"
-
 const KakaoOauthPage = () => {
   // TODO: 카카오 로그인 API 연동 및 로딩 화면 처리
-  const searchParams = useSearchParams()
 
-  const code = searchParams.get("code")
-
-  console.log("Kakao OAuth code:", code)
-
-  return <>{code}</>
+  return <>KakaoOauthPage</>
 }
 
 export default KakaoOauthPage
