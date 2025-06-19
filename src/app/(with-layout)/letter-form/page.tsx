c0d98730830ae6b79102ecf701fc56cdea472b6b
장현봉 <wkdgusqhd080@naver.com>
2025-06-19 22:58:05 +0900

    feat: basic-header implements

---
+++ src/app/(with-layout)/letter-form/page.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+import LetterForm from "@/components/letter-form/letter-form"
+
+const LetterFormPage = () => {
+  return (
+    <>
+      <LetterForm />
+    </>
+  )
+}
+
+export default LetterFormPage
