ac9591714e13d59974ed57279c52126be44cb870
Sinji <sinzi031012@naver.com>
2025-06-25 19:36:55 +0900

    refactor: extract clipboard copy logic into reusable utility function

--- src/components/home/copy-address-button.tsx
+++ src/components/home/copy-address-button.tsx
@@ -4,6 +4,7 @@ import { useState } from "react"
 
 import { IconButton, Tooltip } from "@/components/common"
 import { useSnackbar } from "@/contexts/snackbar"
+import { copyToClipboard } from "@/utils/copy-to-clipboard"
 
 export const CopyAddressButton = () => {
   const [isTooltipOpen, setIsTooltipOpen] = useState(true)
@@ -11,25 +12,12 @@ export const CopyAddressButton = () => {
   const { showSnackbar } = useSnackbar()
 
   const handleClickCopy = async () => {
-    try {
       // TODO : 추후 복사할 url 결정되면 수정
       const tempAddress = "https://doong-doong/~"
 
-      await navigator.clipboard.writeText(tempAddress)
+      copyToClipboard(tempAddress, showSnackbar)
 
-      showSnackbar({
-        message: "주소가 복사되었습니다!",
-        icon: "link",
-      })
-
-      // TODO : 복사 실패 시... 굳이인가 싶기도 해서 얘기 나눠보고 수정하기
-    } catch (error) {
-      console.error("복사 실패:", error)
-      showSnackbar({
-        message: "복사에 실패했습니다. 다시 시도해주세요.",
-        icon: "link",
-      })
-    }
+     
   }
 
   const handleTooltipClose = () => {
