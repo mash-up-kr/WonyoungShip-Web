211f900d41ddf522db65f528936d5a4ddf8e85ed
Pridesd <whwotjr98@naver.com>
2025-06-25 20:42:55 +0900

    feat: add LetterListPage component with BasicHeader

---
+++ src/app/(with-layout)/letter/page.tsx
@@ -0,0 +1,11 @@
+import BasicHeader from "@/components/common/header/basic-header"
+
+const LetterListPage = () => {
+  return (
+    <>
+      <BasicHeader centerText="편지 목록" />
+    </>
+  )
+}
+
+export default LetterListPage
