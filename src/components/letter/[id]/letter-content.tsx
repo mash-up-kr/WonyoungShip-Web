bad469d146dee907c96df3754779823ae6d5bdca
윤신지(콘캣) <sinzi031012@naver.com>
2025-06-29 01:31:19 +0900

    design: apply custom scroll bar

--- src/components/letter/[id]/letter-content.tsx
+++ src/components/letter/[id]/letter-content.tsx
@@ -7,14 +7,13 @@ interface LetterContentProp {
 
 export const LetterContent = ({ content }: LetterContentProp) => {
   return (
-    <section className="bg-background-brandassistive relative mt-4 flex h-[378px] w-full max-w-[343px] flex-col items-center justify-between overflow-hidden rounded-3xl p-6">
+    <section className="bg-background-brandassistive relative mt-4 flex h-[378px] w-full max-w-[343px] flex-col items-center justify-between overflow-hidden rounded-3xl pt-6 pb-6 pl-6 pr-1.5">
       <div
-        className="absolute inset-0 bg-cover bg-center opacity-50"
+        className="absolute inset-0 bg-cover bg-center opacity-90"
         style={{ backgroundImage: `url(${LetterBackground.src})` }}
       />
 
-      {/* TODO : 스크롤바 위치 조정 + 커스텀 하기 */}
-      <div className="flex overflow-y-scroll z-10">
+      <div className="flex overflow-y-scroll z-10 custom-scrollbar pr-5">
         <Text
           variant="body"
           size="large"
