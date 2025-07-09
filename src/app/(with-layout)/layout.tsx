b70f42ba7e3c8e0553236b902e0f5c7bdeb57b6d
Pridesd <whwotjr98@naver.com>
2025-07-09 22:36:25 +0900

    refactor: remove metadata definition from layout component

--- src/app/(with-layout)/layout.tsx
+++ src/app/(with-layout)/layout.tsx
@@ -1,23 +1,4 @@
-import { Metadata } from "next"
 import React from "react"
-
-export const metadata: Metadata = {
-  title: "둥둥 - 느리지만 진심을 담은 편지",
-  description: "미래의 누군가에게 진심을 전하는 디지털 타임캡슐 편지 서비스",
-  openGraph: {
-    title: "둥둥 - 느리지만 진심을 담은 편지",
-    description: "미래의 누군가에게 진심을 전하는 디지털 타임캡슐 편지 서비스",
-    url: "https://doongdoong.org",
-    siteName: "둥둥",
-    images: [
-      {
-        url: "/opengraph-image.png",
-      },
-    ],
-    locale: "ko_KR",
-    type: "website",
-  },
-}
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return <main className="m-auto w-full max-w-[420px]">{children}</main>
