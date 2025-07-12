b7ed6899906af612d7904ce88ee5e2a3a416b6d9
Pridesd <whwotjr98@naver.com>
2025-07-12 15:18:21 +0900

    feat: Implement RegisterTag component with authentication check

--- src/components/tag/register-tag.tsx
+++ src/components/tag/register-tag.tsx
@@ -1,3 +1,15 @@
-export const RegisterTag = () => {
-  return <></>
+import { cookies } from "next/headers"
+
+import { ACCESS_TOKEN_KEY } from "@/constants/cookies"
+
+export const RegisterTag = async () => {
+  const serverCookie = await cookies()
+
+  const token = serverCookie.get(ACCESS_TOKEN_KEY)?.value
+
+  if (!token) {
+    return <>Login</>
+  }
+
+  return <>Register</>
 }
