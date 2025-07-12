bb0c3b60e83d80377f0ea49b217079cd69804fc2
Pridesd <whwotjr98@naver.com>
2025-07-12 14:48:06 +0900

    feat: Add ToastError constants for error handling in LandingToast component

--- src/components/landing/landing-toast.tsx
+++ src/components/landing/landing-toast.tsx
@@ -3,13 +3,14 @@
 import { useSearchParams } from "next/navigation"
 import { useEffect } from "react"
 
+import { ToastError } from "@/constants/toast-error"
 import { useSnackbar } from "@/contexts/snackbar"
 
 export const LandingToast = () => {
   const searchParams = useSearchParams()
   const { showSnackbar } = useSnackbar()
 
-  const error = searchParams.get("error")
+  const error = searchParams.get("error") as ToastError
 
   useEffect(() => {
     if (error === "login") {
@@ -21,6 +22,12 @@ export const LandingToast = () => {
     if (error === "unauthorized") {
       showSnackbar({
         message: "인증이 필요해요. 다시 로그인해주세요.",
+      })
+    }
+
+    if (error === "not-found") {
+      showSnackbar({
+        message: "잘못된 페이지예요.",
       })
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
