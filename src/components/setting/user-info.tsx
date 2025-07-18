fc15a7d1f6413e82015a808775fd7f315ba1df7d
Pridesd <whwotjr98@naver.com>
2025-07-18 22:48:03 +0900

    fix/add-close-dialog

--- src/components/setting/user-info.tsx
+++ src/components/setting/user-info.tsx
@@ -36,6 +36,7 @@ export const UserInfo = () => {
           showSnackbar({
             message: "로그아웃 되었어요",
           })
+          close()
           router.replace("/landing")
         },
       },
